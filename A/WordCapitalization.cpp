9a90b59551dc8a9fd300006a74e8582d2aca4853
JetYap <yyapjet1996@gmail.com>
2021-05-26 17:35:18 +0800

    WordCapitilzation

---
+++ A/WordCapitalization.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    string yuhong;
+    getline(cin, yuhong);
+    if(islower(yuhong[0])){
+        yuhong[0]=toupper(yuhong[0]);
+
+    }
+    cout << yuhong;
+}
